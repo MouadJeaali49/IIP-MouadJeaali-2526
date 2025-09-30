366aadd79fbc4ac31df583cbdaed2b72d5279d25
MouadJeaali49 <mouad.jeaali@student.odisee.be>
2025-09-30 17:53:18 +0200

    ConsoleDrankkaart gemaakt

---
+++ ConsoleDrankkaart/program.cs
@@ -0,0 +1,30 @@
+/*
+* Auteur: Mouad Jeaali
+*Datum: 25/09/2025
+*/
+using System;
+
+namespace ConsoleDrankkaart
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string header = "@====== MENU ======\n"+
+			"☕ Koffie:      3€\n" +
+			"🍵 Thee:        2€\n" +
+			"🥤 Limo:        2€";
+			Console.WriteLine(header);
+			Console.WriteLine("------------------");
+			Console.WriteLine("copyright ©2025");
+			
+			// Lege regel toevoegen
+			Console.WriteLine();
+			
+			Console.WriteLine("Het menu staat ook op \"C:\\Dropbox\\MijnCafe\\menu.pdf\"");
+			
+			// Wacht op toets zodat de console niet direct sluit
+			Console.ReadKey(true);
+		}
+	}
+}	
