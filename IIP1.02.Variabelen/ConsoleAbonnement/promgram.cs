476b89a04a968acd63c07828ad70166a920e5d71
MouadJeaali49 <mouad.jeaali@student.odisee.be>
2025-10-06 08:48:21 +0200

    begonnen aan ConsoleAbbonement

---
+++ IIP1.02.Variabelen/ConsoleAbonnement/promgram.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+
+namespace ConsoleAbonnement
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			
+			Console.Write("Geef je naam: ");
+			string Naam = Console.ReadLine();
+			
+			
+			Console.Write("Bestelgegevens");
+			string header = "@==============\;
+			
+			
+			
+			Console.WriteLine("druk een toets om verder te gaan...");
+			Console.ReadKey(true);
+		}
+	}
+}
