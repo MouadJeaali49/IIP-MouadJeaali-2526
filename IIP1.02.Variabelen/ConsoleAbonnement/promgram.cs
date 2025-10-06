976f1ee2c3312dc33ec2a665aad2c84c374f4593
MouadJeaali49 <mouad.jeaali@student.odisee.be>
2025-10-06 13:55:02 +0200

    ConsoleAbbonnement afgemaakt

--- IIP1.02.Variabelen/ConsoleAbonnement/promgram.cs
+++ IIP1.02.Variabelen/ConsoleAbonnement/promgram.cs
@@ -9,16 +9,46 @@ namespace ConsoleAbonnement
 		{
 			Console.OutputEncoding = Encoding.UTF8;
 			
+			Console.WriteLine("Bestelgegevens");
+			string header = "================\n";
+			Console.WriteLine(header);
 			Console.Write("Geef je naam: ");
 			string Naam = Console.ReadLine();
 			
+			Console.Write("aantal toegangsbeurten: ");
+			int toegangsbeurten = int.Parse(Console.ReadLine());
 			
-			Console.Write("Bestelgegevens");
-			string header = "@==============\;
+			Console.Write("prijs (b.v. 122,5): ");
+			string prijs = Console.ReadLine();
 			
+			Console.Write("badkledij inbegrepen (typ true of false): ");
+            bool inbegrepen = Convert.ToBoolean (Console.ReadLine());
 			
+			Console.Write("geslacht (druk 'm' of 'v'):");
+			char geslacht = Console.ReadKey().KeyChar;
+			Console.WriteLine();
 			
-			Console.WriteLine("druk een toets om verder te gaan...");
+			// Samenvatting tonen
+			Console.WriteLine("");
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Samenvatting");
+			Console.WriteLine("================");
+			
+			Console.WriteLine($"- houder: {Naam}");
+			Console.WriteLine($"- geslacht: {geslacht}");
+			Console.WriteLine($"- prijs: {prijs}");
+			Console.WriteLine($"- aantal beurten: {toegangsbeurten}");
+			Console.WriteLine($"- incl. badkledij: {inbegrepen}");
+			
+			//kaartnummer
+			Random rnd = new Random();
+			string kaartnummer = $"{rnd.Next(100, 1000)}-{rnd.Next(1000, 10000)}-{rnd.Next(10, 100)}"; 
+			Console.WriteLine($"- kaartnummer: {kaartnummer}");
+			Console.ResetColor();
+
+			
+			Console.WriteLine("");
+			Console.WriteLine("druk op een toets om verder te gaan...");
 			Console.ReadKey(true);
 		}
 	}
