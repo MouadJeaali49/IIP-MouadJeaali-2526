8e6b6a4acaf23a8577a786811ffd4cc0a30348c1
MouadJeaali49 <mouad.jeaali@student.odisee.be>
2025-10-18 18:59:48 +0200

    default notepad gemaakt

---
+++ default/program.cs
@@ -0,0 +1,20 @@
+/*
+* Auteur: Mouad Jeaali
+*Datum: xx/xx/2025
+*/
+using System;
+using System.Text;
+using System.Globalization;
+
+namespace xxxxxxx
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			
+			Console.ReadKey();
+		}
+	}
+}
